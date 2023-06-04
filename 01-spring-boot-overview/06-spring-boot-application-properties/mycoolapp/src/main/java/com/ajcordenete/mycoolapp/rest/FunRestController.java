c19f514df9653f65c91e0716ac5f494c45637cd1
ajcordenete <aljon.cordenete@gmail.com>
2023-06-04 21:44:32 +0800

    Add demo for custom application properties

--- 01-spring-boot-overview/06-spring-boot-application-properties/mycoolapp/src/main/java/com/ajcordenete/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/06-spring-boot-application-properties/mycoolapp/src/main/java/com/ajcordenete/mycoolapp/rest/FunRestController.java
@@ -1,24 +1,10 @@
 package com.ajcordenete.mycoolapp.rest;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-
-    //Inject properties for coach.name and team.name
-    @Value("${coach.name}")
-    public String coachName;
-
-    @Value("${team.name}")
-    public String teamName;
-
-    // expose new endpoint for teamInfo
-    @GetMapping("/team")
-    public String getTeamInfo() {
-        return "Coach:" + coachName + ", Team:" + teamName;
-    }
 
     // expose "/" that returns "Hello World"
     @GetMapping("/")
