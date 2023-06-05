69903e529b1b75eee2ae7e371111723be37ae2d1
ajcordenete <aljon.cordenete@gmail.com>
2023-06-05 14:48:31 +0800

    Add demo for setter injection

---
+++ 02-spring-boot-springcore/03-spring-boot-setter-injection/src/main/java/com/ajcordenete/springcoredemo/rest/DemoRestController.java
@@ -0,0 +1,23 @@
+package com.ajcordenete.springcoredemo.rest;
+
+
+import com.ajcordenete.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
