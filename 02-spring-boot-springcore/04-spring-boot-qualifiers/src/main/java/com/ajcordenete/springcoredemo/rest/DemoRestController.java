5d081d6b2e5c74bb3291ed15a94294e2acd3cbaf
ajcordenete <aljon.cordenete@gmail.com>
2023-06-05 15:02:01 +0800

    Add demo for using qualifier annotation for di

---
+++ 02-spring-boot-springcore/04-spring-boot-qualifiers/src/main/java/com/ajcordenete/springcoredemo/rest/DemoRestController.java
@@ -0,0 +1,24 @@
+package com.ajcordenete.springcoredemo.rest;
+
+
+import com.ajcordenete.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoRestController(@Qualifier("baseballCoach") Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
